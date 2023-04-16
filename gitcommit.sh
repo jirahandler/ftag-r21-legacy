cc628de7dbb4fce9c091749687dfaf14ae7766e8
Soumyananda Goswami <sgoswami@cern.ch>
2023-04-17 00:37:17 +0200

    00:37:17 commit

--- gitcommit.sh
+++ gitcommit.sh
@@ -1,6 +1,6 @@
-git init
+#git init
 git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:jirahandler/ftag-r21-legacy.git
+git commit -m "$(date +'%H:%M:%S') commit"
+#git branch -M main
+#git remote add origin git@github.com:jirahandler/ftag-r21-legacy.git
 git push -u origin main
